cb8477d17de73e47bea11e527a5bfcedaaee9512
batan <122584289+batann@users.noreply.github.com>
2025-01-19 22:03:01 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
+clear
 cd /home/batan/
-sudo mv anon /usr/share/plymouth/themes/
+if [[ -d == /usr/share/lc-plymouth/themes/anon ]];
+then
+sudo mv /usr/share/lc-plymouth/themes/anon /usr/share/lc-plymouth/themes/anon.org
+fi
+sudo mv /home/batan/lc-plymouth/anon /usr/share/plymouth/themes/
 cd /usr/share/plymouth/themes
 sudo plymouth-set-default-theme -R anon
 sudo update-initramfs -u
 sudo update-grub2
+cd ~
+sudo trash /home/batan/lc-plymouth
+clear
+echo -e "\033[32mScript executed successfully"
+echo -e "\033[32mExiting...\033[0m"
